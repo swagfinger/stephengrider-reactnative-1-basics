1e8c86bdbea1efc2f962adf769b466295529c2b7
Swag Finger <swagfinger@gmail.com>
2022-11-09 01:22:28 +0800

    flex-box / position

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,21 +3,45 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Box Screen</Text>
+    <View style={styles.parentStyle}>
+      <Text style={styles.viewOneStyle}>Child 1</Text>
+      <Text style={styles.viewTwoStyle}>Child 2</Text>
+      <Text style={styles.viewThreeStyle}>Child 3</Text>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  viewStyle: {
+  parentStyle: {
     borderWidth: 3,
-    borderColor: 'black'
+    borderColor: 'black',
+    height: 200,
+    flexDirection: 'row', //column or row //default:column
+    //alignItems: 'center', //default: stretch , flex-start, flex-end  //put on parent element
+    justifyContent: 'space-between' //put on children 'flex-start', 'flex-end', 'justify-content', 'space-around'
+  },
+  viewOneStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'red'
+  },
+  viewTwoStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'green',
+    top: 50
+  },
+  viewThreeStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'purple'
   },
   textStyle: {
     borderWidth: 1,
-    borderColor: 'red',
-    marginVertical: 20,
-    marginHorizontal: 20
+    borderColor: 'red'
+    // marginVertical: 20,
+    // marginHorizontal: 20,
+
+    // ...StyleSheet.absoluteFillObject
   }
 });
 
