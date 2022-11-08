6235c6a13b4ef87d5e842440a22b1a92c8b2a1e2
Swag Finger <swagfinger@gmail.com>
2022-11-08 15:02:01 +0800

    changing counter component to useReducer()

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,17 +1,36 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+const reducer = (state, action) => {
+  //state === {counter:number}
+  //action === {type:'increment' || 'decrement', payload:1}
+  switch (action.type) {
+    case 'increment':
+      return { ...state, counter: state.counter + action.payload };
+    case 'decrement':
+      return { ...state, counter: state.counter - action.payload };
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
+  const { counter } = state;
   return (
     <View>
-      <Button title="incr" onPress={() => setCounter(counter + 1)} />
-      <Button title="decr" onPress={() => setCounter(counter - 1)} />
+      <Button
+        title="incr"
+        onPress={() => dispatch({ type: 'increment', payload: 1 })}
+      />
+      <Button
+        title="decr"
+        onPress={() => dispatch({ type: 'decrement', payload: 1 })}
+      />
       <Text>Current Count: {counter}</Text>
     </View>
   );
-
-  const styles = StyleSheet.create({});
 };
+const styles = StyleSheet.create({});
 
 export default CounterScreen;
